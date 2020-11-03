0564da640cb6a6cbc0341289f5f003e6a615765c
Bsh <tmdgjsqor1997@naver.com>
2020-11-04 01:45:42 +0900

    Chset 추가
    진짜 잔다.

--- app/src/main/java/com/example/my_project/GymActivity.java
+++ app/src/main/java/com/example/my_project/GymActivity.java
@@ -3,18 +3,28 @@ package com.example.my_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GymActivity extends AppCompatActivity {
 
-    Button pre_btn;
+    ImageButton chest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym);
 
+        chest=(ImageButton)findViewById(R.id.chest);
+        chest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(GymActivity.this, ChestActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
